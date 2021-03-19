0f5799459bea6b490425bc00cc5d84ff21febe3d
Marcus Kim <marcuskim1989@gmail.com>
2021-03-18 20:50:36 -0700

    Initial commit

---
+++ app/src/main/java/com/marcuskim/todude/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.marcuskim.todude;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.marcuskim.todude.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
