e643358d8d45fae0c316d61c6544017c97388ce2
Marcus Kim <marcuskim1989@gmail.com>
2021-03-24 15:08:11 -0700

    set up due date chips and enable use of calendar to pick due date

--- app/src/main/java/com/marcuskim/todude/BottomSheetFragment.java
+++ app/src/main/java/com/marcuskim/todude/BottomSheetFragment.java
@@ -25,7 +25,7 @@ import androidx.constraintlayout.widget.Group;
 import java.util.Calendar;
 import java.util.Date;
 
-public class BottomSheetFragment extends BottomSheetDialogFragment {
+public class BottomSheetFragment extends BottomSheetDialogFragment implements View.OnClickListener {
 
     private EditText enterTodo;
     private ImageButton calendarButton;
@@ -56,8 +56,11 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
         priorityRadioGroup = view.findViewById(R.id.radioGroup_priority);
 
         Chip todayChip = view.findViewById(R.id.today_chip);
+        todayChip.setOnClickListener(this);
         Chip tomorrowChip = view.findViewById(R.id.tomorrow_chip);
+        tomorrowChip.setOnClickListener(this);
         Chip nextWeekChip = view.findViewById(R.id.next_week_chip);
+        nextWeekChip.setOnClickListener(this);
 
         return view;
 
@@ -89,5 +92,27 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.today_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR, 0);
+            dueDate = calendar.getTime();
+            Log.d("TIME", "onClick: " + dueDate.toString());
+
+        } else if (id == R.id.tomorrow_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+            dueDate = calendar.getTime();
+            Log.d("TIME", "onClick: " + dueDate.toString());
+
+        } else if (id == R.id.next_week_chip) {
+            calendar.add(Calendar.DAY_OF_YEAR, 7);
+            dueDate = calendar.getTime();
+            Log.d("TIME", "onClick: " + dueDate.toString());
+
+        }
     }
 }
